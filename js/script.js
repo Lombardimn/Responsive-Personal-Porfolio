acd6b46d773bd20c4f43084ece2185810d31abc8
Matias Lombardi <matiiaslombardi@gmail.com>
2023-05-16 03:51:17 -0300

    V.1.7.0 - Navbar struture modificaction + responsive + info home

--- js/script.js
+++ js/script.js
@@ -9,23 +9,29 @@ menuIcon.onclick = () => {
 
 // Scroll sections
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let navLinks = document.querySelectorAll('header nav div a');
+let iconLinks = document.querySelectorAll('header nav div i');
 
 window.onscroll = () => {
-sections.forEach(sec =>{
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 100;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-    if(top >= offset && top < offset + height) {
-        // active navbar links
-        navLinks.forEach(links => {
-            links.classList.remove('active');
-            document.querySelector('header nav a[href*='+ id +']').classList.add('active');
-        });
-    }
-});
+        if(top >= offset && top < offset + height) {
+            // active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id +']').classList.add('active');
+            });
+
+            iconLinks.forEach(links1 => {
+                links1.classList.remove('active');
+                document.querySelector('header nav i[id*='+ id +']').classList.add('active');
+            });
+        }
+    });
 
     // sticky header
     let header = document.querySelector('header');

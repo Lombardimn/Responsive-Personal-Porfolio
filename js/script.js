df88a4c8b8704fd8d5b5d2fc1d2a60237887fe3b
Matias Lombardi <matiiaslombardi@gmail.com>
2023-05-17 05:19:04 -0300

    V.1.7.1 - Intregated animation (logic and keyframe) + fixed responsive

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toggle icon navbar
+// TOGGLE ICON NAVBAR
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar')
 
@@ -7,7 +7,7 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 }
 
-// Scroll sections
+// SCROLL SECTIONS
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav div a');
 let iconLinks = document.querySelectorAll('header nav div i');
@@ -20,7 +20,7 @@ window.onscroll = () => {
         let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height) {
-            // active navbar links
+            // ACTIVE NAVBAR LINKS
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*='+ id +']').classList.add('active');
@@ -30,11 +30,28 @@ window.onscroll = () => {
                 links1.classList.remove('active');
                 document.querySelector('header nav i[id*='+ id +']').classList.add('active');
             });
+
+            // ACTIVE SECTIONS FOR ANIMATION ON SCROLL
+            sec.classList.add('show-animate');
+        }
+
+        // TO USE THE ANIMATION THAT REPEATS ON SCROLLING USE THIS
+        else{
+            sec.classList.remove('show-animate');
         }
     });
 
-    // sticky header
+    // STICKY HEADER
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // REMOVE TOGGLE ICON AND NAVBAR WHEN CLICK NAVBAR LINKS (SCROLL)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    // ANIMATION FOOTER ON SCROLL
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }

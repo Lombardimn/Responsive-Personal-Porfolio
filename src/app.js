7eeafdfaae07dd60db96bab31618b12710f7b857
Matias Lombardi <matiiaslombardi@gmail.com>
2023-06-09 00:11:27 -0300

    V.2.2.3 - Fixed stactic directory public v_2

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ import indexRoutes from './routes/index.js'
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// app.use(express.static('public'));
-app.use('/static', express.static('public'));
+app.use(express.static('public'));
+// app.use('/static', express.static('public'));
 
 
 app.use(indexRoutes);
